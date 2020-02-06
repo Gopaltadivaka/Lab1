40fdf323d6f57d01f9d028740115a3b96e0a4b4e
gopal <gopal@LAPTOP-THDP34FM>
2020-02-05 20:14:24 -0600

    varible names changed

--- src/Cookies.java
+++ src/Cookies.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class Cookies {
 	public static void main(String[] args) {
-		double numberofcookies;
+		double numberOfCookies;
 		Scanner sc= new Scanner(System.in);
-		double Cpc=75;//calories per cookie
+		double cpc=75;//calories per cookie
 		System.out.println("Enter the number of cookies ate:");
-		 numberofcookies =sc.nextDouble();
-		double caloriesconsumed= numberofcookies*Cpc;//compute the calories consumed 
-		System.out.println("Calories consumed:"+ caloriesconsumed);
+		numberOfCookies =sc.nextDouble();
+		double caloriesConsumed= numberOfCookies*cpc;//compute the calories consumed 
+		System.out.println("Calories consumed:"+ caloriesConsumed);
 		sc.close();
 	}
 
