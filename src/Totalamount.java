6ba1fdf8bb9684138786832567768bb13f4a68da
gopal <gopal@LAPTOP-THDP34FM>
2020-02-17 16:57:45 -0600

    changesmade to display totalsaletax

--- src/Totalamount.java
+++ src/Totalamount.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 public class Totalamount {
 public static void main(String[] args) {
-	double purchaseamount,Aacst,Aacct,totalsaleamount;
-	double statetax=0.04;
-	double countrytax=0.02;
+	double purchaseAmount,Aacst,Aacct,totalsaletax,totalSaleAmount;
+	double stateTax=0.04;
+	double countryTax=0.02;
 	Scanner sc =new Scanner(System.in);
 	System.out.println("enter the purchase amount:");//ask the user to enter the purchase amount
-	purchaseamount= sc.nextDouble();
-	 Aacst =purchaseamount*statetax;//compute the state tax
-	 Aacct =purchaseamount*countrytax;//compute the country tax
-	 totalsaleamount = purchaseamount+Aacst+Aacct;//compute the total sale amount
-	System.out.println("purchaseamount:\t"+purchaseamount +"\nstatetax:\t"+Aacst+"\ncountrytax:\t"+Aacct +"\ntotalsaleamount:\t" + totalsaleamount);//display the purchase amount,state tax,country tax,sale amount
+	purchaseAmount= sc.nextDouble();
+	 Aacst =purchaseAmount*stateTax;//compute the state tax
+	 Aacst =purchaseAmount*countryTax;//compute the country tax
+	 totalsaletax = Aacst + Aacst;//totalsaletax
+	 totalSaleSmount = purchaseAmount+Aacst+Aacct;//compute the total sale amount
+	System.out.println("purchaseamount:\t"+purchaseamount +"\nstateTax:\t"+Aacst+"\ncountryTax:\t"+Aacct +"\ntotalsaletax:"+totalsaletax+"\ntotalSaleAmount:\t" + totalsaleAmount);//display the purchase amount,state tax,country tax,sale amount
 	sc.close();
 }
 }
